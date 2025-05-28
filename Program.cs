b870ada214a61f3ff53ce317f7c949118f521616
3jamees <jamesiiking8@gmail.com>
2025-05-28 10:18:24 -0400

    fourth commit

--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-int a = 5;
+int a = 7;
 int b = 4;
-int c = 2;
-int d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
-Console.WriteLine(d);
-d = (a + b) * c;
+int c = 3;
+int d = (a + b) / c;
 Console.WriteLine(d);
