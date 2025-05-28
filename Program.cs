102fa1548a8e5b2b8b6aee24c6112e8935d08da3
3jamees <jamesiiking8@gmail.com>
2025-05-28 10:29:59 -0400

    seventh commit

--- Program.cs
+++ Program.cs
@@ -1,14 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-int a = 7;
-int b = 4;
-int c = 3;
-int d = (a + b) / c;
-int e = (a + b) % c;
-Console.WriteLine($"quotient: {d}");
-Console.WriteLine($"remainder: {e}");
-int max = int.MaxValue;
-int min = int.MinValue;
-Console.WriteLine($"The range of integers is {min} to {max}");
-int what = max + 3;
-Console.WriteLine($"An example of overflow: {what}");
+double a = 5;
+double b = 4;
+double c = 2;
+double d = (a + b) / c;
+Console.WriteLine(d);
